6e54850653629ecefe5be31657f54ad30bbaff73
Christina Golla <cgolla@techfak.uni-bielefeld.de>
2018-05-08 17:20:06 +0200

    App can track markers now :D

---
+++ app/src/main/assets/Test/js/custom.js
@@ -0,0 +1,101 @@
+var World = {
+	loaded: false,
+
+	init: function initFn() {
+		this.createOverlays();
+	},
+
+	createOverlays: function createOverlaysFn() {
+
+		/*
+			First an AR.ImageTracker needs to be created in order to start the recognition engine.
+			It is initialized with a AR.TargetCollectionResource specific to the target collection
+			that should be used. Optional parameters are passed as object in the last argument.
+			In this case a callback function for the onTargetsLoaded trigger is set.
+			Once the tracker loaded all its target images, the function worldLoaded() is called.
+
+			Important: If you replace the tracker file with your own, make sure to change the target
+			name accordingly.
+			Use a specific target name to respond only to a certain target or use a wildcard to
+			respond to any or a certain group of targets.
+
+			Adding multiple targets to a target collection is straightforward.
+			Simply follow our Target Management Tool documentation. Each target in the target
+			collection is identified by its target name. By using this target name, it is possible
+			to create an AR.ImageTrackable for every target in the target collection.
+		*/
+		this.targetCollectionResource = new AR.TargetCollectionResource("assets/test_targets.wtc", {
+		});
+
+		this.tracker = new AR.ImageTracker(this.targetCollectionResource, {
+			onTargetsLoaded: this.worldLoaded,
+            onError: function(errorMessage) {
+            	alert(errorMessage);
+            }
+		});
+
+		/*
+			The next step is to create the augmentation. In this example an image resource is created and passed to the AR.ImageDrawable. A drawable is a visual component that can be connected to an IR target (AR.ImageTrackable) or a geolocated object (AR.GeoObject). The AR.ImageDrawable is initialized by the image and its size. Optional parameters allow for position it relative to the recognized target.
+		*/
+
+		// Create overlay for page one
+
+		var imgOne = new AR.ImageResource("assets/wimbledon1_overlay2.png");
+        var overlayOne = new AR.ImageDrawable(imgOne, 1.0, {
+        	translate: {
+        		x: 0,
+        		y: 0
+        	}
+        });
+
+		// add drawable to marker
+		var pageOne = new AR.ImageTrackable(this.tracker, "wimbledon1", {
+		    drawables: {
+		        cam: overlayOne
+		    },
+		    onImageRecognized: this.removeLoadingBar,
+                        onError: function(errorMessage) {
+                        	alert(errorMessage);
+                        }
+		});
+
+		/*
+			Similar to the first part, the image resource and the AR.ImageDrawable for the second overlay are created.
+		*/
+		var imgTwo = new AR.ImageResource("assets/wimbledon2_overlay.png");
+		var overlayTwo = new AR.ImageDrawable(imgTwo, 1.0, {
+			translate: {
+				x: 0,
+				y: 0
+			}
+		});
+
+		/*
+			The AR.ImageTrackable for the second page uses the same tracker but with a different target name and the second overlay.
+		*/
+		var pageTwo = new AR.ImageTrackable(this.tracker, "wimbledon2", {
+			drawables: {
+				cam: overlayTwo
+			},
+			onImageRecognized: this.removeLoadingBar,
+            onError: function(errorMessage) {
+            	alert(errorMessage);
+            }
+		});
+	},
+
+	removeLoadingBar: function() {
+		if (!World.loaded) {
+			var e = document.getElementById('loadingMessage');
+			e.parentElement.removeChild(e);
+			World.loaded = true;
+		}
+	},
+
+    //Once the tracker loaded all its target images, the function worldLoaded() is called.
+	worldLoaded: function worldLoadedFn() {
+        document.getElementById("test").innerHTML = "The world is loaded!!";
+	}
+};
+
+World.init();
--- app/src/main/java/waldemarapp/waldemar/ViewController/MainActivity.java
+++ app/src/main/java/waldemarapp/waldemar/ViewController/MainActivity.java
@@ -1,7 +1,18 @@
 package waldemarapp.waldemar.ViewController;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wikitude.architect.ArchitectStartupConfiguration;
+import com.wikitude.architect.ArchitectView;
+
+import java.io.IOException;
+
 import waldemarapp.waldemar.R;
 
 /**
@@ -10,16 +21,77 @@ import waldemarapp.waldemar.R;
 public class MainActivity extends AppCompatActivity {
 
     /*========== VARS ===============================*/
+    private static final String WIKITUDE_KEY = "Hp/fXCJzZARFV7BHmgN9jCn6OBerUtUPX4mF+2gHUV8zfq1tyxUWdrrnGodkDrM5mcqfq8H5udGDcHVJ/M/Hvezoa9nH0BaMpW/fRiJxFr/A4SWaL5Rokl80uazjrJvGX1WrE/OGVVKYkwO9N33KPbZ0MbHp4chQB95Z7M8k1kNTYWx0ZWRfX6XJ+mvnPZ8m15Lu5AQutwKqPx4HGlN1S2rmFw81DERjkY3l2M8Ek4fiK7cMPw2eH6BiBihHRi1a2eGBZ1eA6v8yONwKVSdPp6ScIClHTi94tRfixwg3ORDh/eWIKUJF49mTzMw6D+iqekHmWK3l4V6MUuNCmYvkzTz8gRFQMe+QAspnV50k9guTiIflynztBnIZc8XdfUMBKra8HMLyKqUuRqj0OLs3Biv+jAxVHJvJdgo2BcLAaXVysySImAwY/oAvmf5+x/xkaAW0excDl2mdEbMVaPd2nPx9/SCAGxwXPefyMKmdVwuO1A8EP4aY69wFKzzzJJbYl4efOjv0QCvRhaRTGsnCz2maJTwZIewO7NvZ6GivMBK6F8HiZKeN/32vfnWx2qtAUjnjMchqVGK3schnSpTfGhkX5qMSwUkxqX3iv6VI6dsUMj0i15m8tGDK/xiLlan8D4vtoEFeVJQgoexYnFWS9HROybQWFH1JnFkdCw0NnWX+4LN7Fd9rUJQPDr/KRLKs3Pl/sCnlq3HMYmikqSXTMg==";
+
+    private ArchitectView architectView;
 
     /*========== CUSTOM FUNCTIONS ===============================*/
 
+    /**
+     * You need to check and gain the camera permission to use the architectView.
+     */
+    private void cameraPermission() {
+        // TODO: check why this doesnt work
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA);
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            // you have the permission, everything is fine
+        } else {
+            // you do not have the permission, so ask for it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    permissionCheck);
+        }
+    }
+
     /*========== LIFE CYCLE EVENTS ===============================*/
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Init Wikitude
+        this.architectView = (ArchitectView)this.findViewById( R.id.architectView_main );
+        final ArchitectStartupConfiguration config = new ArchitectStartupConfiguration();
+        config.setLicenseKey(WIKITUDE_KEY);
+        cameraPermission();
+        this.architectView.onCreate( config );
+
     }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        architectView.onPostCreate();
+        try {
+            architectView.load("Test/index.html");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("unable to load html file: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        architectView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        architectView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        architectView.onResume();
+    }
+
+
 
     /*========== GETTER & SETTER ===============================*/
 }
