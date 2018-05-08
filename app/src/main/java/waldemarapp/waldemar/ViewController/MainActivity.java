0785148c91bfd9a19f90b75a29fbda9094234737
Christina Golla <cgolla@techfak.uni-bielefeld.de>
2018-05-08 16:11:27 +0200

    Built layout for MainActivity

---
+++ app/src/main/java/waldemarapp/waldemar/ViewController/MainActivity.java
@@ -0,0 +1,25 @@
+package waldemarapp.waldemar.ViewController;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import waldemarapp.waldemar.R;
+
+/**
+ * The MainActivity where markers can be scanned.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /*========== VARS ===============================*/
+
+    /*========== CUSTOM FUNCTIONS ===============================*/
+
+    /*========== LIFE CYCLE EVENTS ===============================*/
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*========== GETTER & SETTER ===============================*/
+}
