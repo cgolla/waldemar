80fdc8260f7844b08c770e3fcb7280011658bd07
Christina Golla <cgolla@techfak.uni-bielefeld.de>
2018-06-19 16:28:56 +0200

    Iggy's game basically finished

--- app/src/main/assets/prototyp/games/iggy/js/iggy.js
+++ app/src/main/assets/prototyp/games/iggy/js/iggy.js
@@ -1,9 +1,13 @@
+// ============ VARS
+	var shakeCount = 0;   // counter for shakes
+	var shakeCountTarget = 29; // targeted amount of shakes (roughly x 10)
+	var shakeHeight = 10; // which acceleration-value counts as a "shake"?
+	var shakeDuration = 0.5; // duration of shaking-animation in seconds
+
+
+// ============ DOCUMENT READY
 $(document).ready(function(){
-
-	var shakeCount = 0;
-	var shakeHeight = 10;
-
-		
+	
 	deviceMotionHandler = function(evt){
 			
 		// console.log("HELLO "+evt.acceleration.x);
@@ -12,10 +16,14 @@ $(document).ready(function(){
 			  // jumpMax.x = evt.acceleration.x;
 			  console.log("I AM SHOOKETH "+shakeCount+" "+evt.acceleration.x);
 			  shakeCount = shakeCount+1;
+			  shakeApple();
 
-			  if(shakeCount == 29){
+			  // WINNING
+			  if(shakeCount == shakeCountTarget){
 					console.log("I WAS SHOOK AT LEAST 3 TIMES!");
 					window.removeEventListener('devicemotion', deviceMotionHandler);
+					dropApple();
+					$(".notification-wrap").delay(1200).show("fast");
 			  }
 			}
 			
@@ -23,12 +31,49 @@ $(document).ready(function(){
 
 	}
 	
+	// falls DeviceMotion unterstützt ist:
 	if (window.DeviceMotionEvent) {
-	  // window.addEventListener('devicemotion', deviceMotionHandler);
+	  console.log("Device motion is supported.");
 	  window.addEventListener('devicemotion', deviceMotionHandler);
-
-	  //setTimeout(stopJump, 3*1000);
-	  
+	}
+	// falls nicht, reagier mal auf Klick
+	else {
+		console.log("Device motion not supported.");
+		$("body").on("click",function(){
+			shakeApple();
+		});
 	}
 
 });
+
+
+// ============== FUNCTIONS
+/**
+* Funktion bringt $(".apfel") zum Wackeln.
+*/
+function shakeApple(){
+	$(".apfel").css("animation","shake "+shakeDuration+"s linear 0s 2 alternate");
+
+	setTimeout(function() {
+		$(".apfel").css("animation","none");
+	}, shakeDuration*2000);
+}
+
+
+/**
+* Funktion bringt $(".apfel") zum "Herunterfallen".
+* Dazu wird das Original-Div ausgeblendet und ein anderes eingeblendet, dass
+* in der HTML-Struktur besser zur Animation geeignet ist.
+*/
+function dropApple(){
+	$(".apfel").hide();
+	$(".apfel-falling").show();
+
+	var appleHeight = $(".apfel").height();
+	console.log("ApfelHöhe: "+appleHeight);
+	var appleTop = "calc(100% - "+appleHeight+"px)";
+
+	$(".apfel-falling").css({
+		top: appleTop
+	});
+}
